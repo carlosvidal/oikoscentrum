970145d9232d01223ab30006491c4c543e34140f
Carlos Vidal <carlos@mitienda.pe>
2024-01-03 21:55:26 -0500

    vite 1

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# oikoscentrum
+# Vue 3 + Vite
 
-Oikos Centrum Website
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
-# https://sprucecss.com/blog/the-simplest-sass-compile-setup/
+## Recommended IDE Setup
 
-# npm run sass-prod
-
-# npm run sass-dev
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <!-- AUTH -->
+    <div class="user-session">
+        <div class="msg">
+        </div>
+        <form id="keep-me-posted" class="keep-me-posted" method="post" action="http://localhost:8888/keep_me_posted/signup">
+            <input type="hidden" name="current_uri" value="http://localhost:8888/oikoscentrum">
+            <div class="InputAddOn">
+                <input type="text" id="email" name="email" placeholder="Email address">
+                <button class="btn">Keep me posted</button>
+                <div data-lastpass-icon-root="true"
+                    style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;">
+                </div>
+            </div>
+        </form>
+        <a href="#login-modal" rel="modal:open" class="btn">Login</a>
+        <a href="http://localhost:8888/signup" class="btn">Sign up</a>
+
+    </div>
+    <!-- NAVEGACION -->
+    <nav class="navegacion">
+        <a href="#oikoscentrum" class="navegacion__item navegacion__item-oikos">
+            <strong>OIKOS</strong><span>CENTRUM</span> </a>
+        <a href="#incubator" class="navegacion__item navegacion__item-incubator"> <strong>INCUBATOR</strong><span>BASIC
+                SOCIAL
+                RESEARCH</span> </a>
+        <a href="#laboratory" class="navegacion__item navegacion__item-laboratory">
+            <strong>LABORATORY</strong><span>APPLIED SOCIAL RESEARCH</span> </a>
+        <a href="#antenna" class="navegacion__item navegacion__item-antena"> <strong>ANTENNA</strong><span>CULTURAL
+                ACTION &
+                EDUCATION</span> </a>
+        <a href="#campaings" class="navegacion__item navegacion__item-climateactionnow">
+            <span>CLIMATE</span><span class="color-rojo">ACTION</span><span>NOW!</span> </a>
+    </nav>
+</template>
