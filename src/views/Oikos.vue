1d17198d640efe097d33f3718eb846f284d67afb
Carlos Vidal <carlos@mitienda.pe>
2024-01-05 19:37:35 -0500

    bien

--- src/views/Oikos.vue
+++ src/views/Oikos.vue
@@ -423,5 +423,41 @@
 </template>
 
 <script setup>
+import { onBeforeMount, onMounted, onUpdated, ref } from "vue";
+//equivalente a document.ready de jquery
+// window.addEventListener("DOMContentLoaded", (event) => {
+onMounted(() => {
+    console.log('DOMContentLoaded');
 
+    // map our commands to the classList methods
+    const fnmap = {
+        'toggle': 'toggle',
+        'show': 'add',
+        'hide': 'remove'
+    };
+    const collapse = (selector, cmd) => {
+        const targets = Array.from(document.querySelectorAll(selector));
+        targets.forEach(target => {
+            target.classList[fnmap[cmd]]('show');
+        });
+    }
+
+    // Grab all the trigger elements on the page
+    const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+    // Listen for click events, but only on our triggers
+    window.addEventListener("click", callback);
+    window.addEventListener("touchstart", callback);
+
+    function callback(ev) {
+
+        console.log('click');
+
+        const elm = ev.target;
+        if (triggers.includes(elm)) {
+            const selector = elm.getAttribute('data-target');
+            collapse(selector, 'toggle');
+        }
+    };
+});
 </script>
